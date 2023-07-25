b76e54a878f2f4d41232b98c2d53674dd2175e15
Egor Orlenok <messaf@mail.ru>
2023-07-25 21:31:01 +1000

    update lesson 06

--- JavaHomeWorks/src/Lesson_06/Main.java
+++ JavaHomeWorks/src/Lesson_06/Main.java
@@ -11,18 +11,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Book> bookList = new ArrayList<>(Arrays.asList(
+        Library library = new Library(Arrays.asList(
                 new Book("Name1", "BookAuthor1", new GenreBook("триллер"),1999),
                 new Book("Name2", "BookAuthor3", new GenreBook("драмма"),1999),
                 new Book("Name3", "BookAuthor2", new GenreBook("триллер"),2022),
                 new Book("Name4", "BookAuthor3", new GenreBook("комедия"),1968),
                 new Book("Name5", "BookAuthor1", new GenreBook("триллер"),1999),
-                new ElectronBook("Name6", "BookAuthor2", new GenreBook("комедия"),2000, "TEXT", 1024),
+                new ElectronBook("Name6", "BookAuthor2", new GenreBook("драмма"),2000, "TEXT", 1024),
                 new ElectronBook("Name7", "BookAuthor3", new GenreBook("комедия"),2001, "PDF", 512),
-                new ElectronBook("Name8", "BookAuthor3", new GenreBook("комедия"),2003, "JSON", 1024),
-                new ElectronBook("Name9", "BookAuthor1", new GenreBook("комедия"),2003, "HTML", 2048)
+                new ElectronBook("Name8", "BookAuthor3", new GenreBook("драмма"),2003, "JSON", 1024),
+                new ElectronBook("Name9", "BookAuthor1", new GenreBook("триллер"),2003, "HTML", 2048)
         ));
-
-        Library library = new Library(bookList);
     }
 }
